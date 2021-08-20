df5a2cdb0d6a1dd612e4c6056230fa8486791e5c
serverkim <serverkim@ntrance.co.kr>
2021-08-20 16:47:47 +0900

    서버 구조 변경에 따른 변경사항 적용. - server id range 변경. - server list에서 server type 삭제.

--- server/dist/services/server-list-service.js
+++ server/dist/services/server-list-service.js
@@ -5,15 +5,14 @@ class ServerListService {
     constructor() {
         this.serverList = new Map();
         this.outDateTime = 15000;
-        this.maxServerId = 255;
-        this.outDateServerId = 100;
+        this.maxServerId = 511;
+        this.outDateServerId = 31;
     }
     close() {
     }
     SetServer(serverInfo) {
         const server = {
             serverId: serverInfo.serverId,
-            serverType: serverInfo.serverType,
             maxUserCount: serverInfo.maxUserCount,
             currentUserCount: serverInfo.currentUserCount,
             createCharacterAvailable: serverInfo.createCharacterAvailable,
@@ -32,10 +31,9 @@ class ServerListService {
                 continue;
             }
             serverInfo.outDate = (curDate.getTime() - serverInfo.updateDate.getTime()) > this.outDateTime ? true : false;
-            if (!(serverInfo.serverId >= this.outDateServerId && serverInfo.outDate)) {
+            if (!(serverInfo.serverId > this.outDateServerId && serverInfo.outDate)) {
                 retServerList[key] = {
                     serverId: serverInfo.serverId,
-                    serverType: serverInfo.serverType,
                     maxUserCount: serverInfo.maxUserCount,
                     currentUserCount: serverInfo.currentUserCount,
                     createCharacterAvailable: serverInfo.createCharacterAvailable,
