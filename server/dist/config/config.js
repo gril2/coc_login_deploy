4e36ff1e54492bccc1202bf0e65d6223212447d0
grilnim <grilnim@gmail.com>
2023-07-19 11:30:51 +0900

    qa용 유틸 서버 주소 변경

--- server/dist/config/config.js
+++ server/dist/config/config.js
@@ -15,7 +15,7 @@ exports.util_server_api_key = 'PjrHQj6pVDXb5s1I4kpXJ/uxUG3a7ORLeH+YT61ZIYs=';
 exports.api_url_dev = 'http://192.168.0.231:13010/config';
 exports.api_url_prod = 'http://13.124.70.168:13010/config';
 exports.api_url_bot = 'http://192.168.0.231:13010/config';
-exports.api_url_qa = 'http://192.168.0.231:13010/config';
+exports.api_url_qa = 'http://192.168.0.151:13010/config';
 exports.getDBConfigUrl = () => {
     if (process.env.NODE_ENV === 'aws_qa') {
         return exports.api_url_qa;
